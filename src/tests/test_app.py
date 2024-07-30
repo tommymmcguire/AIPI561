08db59aa2b0da8ce2fe548277a350da2a0dd7101
tommymmcguire <tmm126@duke.edu>
2024-07-30 15:13:00 -0500

    fix lint

--- src/tests/test_app.py
+++ src/tests/test_app.py
@@ -1,6 +1,7 @@
 import unittest
 from src.app import app
 
+
 class AppTestCase(unittest.TestCase):
     def setUp(self):
         self.app = app.test_client()
@@ -8,6 +9,7 @@ class AppTestCase(unittest.TestCase):
     def test_health_check(self):
         response = self.app.get('/')
         self.assertEqual(response.status_code, 200)
+
 
 if __name__ == '__main__':
     unittest.main()
